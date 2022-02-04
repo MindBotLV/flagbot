fe155c9b613e546b4fc8cd6e1d258ebd8e05da65
MindBotLV <94404530+MindBotLV@users.noreply.github.com>
2022-02-04 11:55:29 -0500

    Update flagbot.js

--- flagbot.js
+++ flagbot.js
@@ -10,7 +10,7 @@ dotenv.config();
 var responses = ["I can't read that, try learning the wave library","I cant understand you very well, come back when you have something from the file I might be able to read, like base64","Spamming me won't do much, well, it might give a tiny hint","Go poke around in the file or view it differently, maybe in a spectrogram"];
 
 client.on('ready', () => {
-	client.user.setActivity("DM Only");
+	client.user.setActivity("Dm ^file for challenge file");
 	console.log ('Logged in as ${H4ck50n}!');
 });
 
@@ -22,6 +22,8 @@ client.on('messageCreate', message => {
 			message.channel.send(process.env.HINT);
 		}else if(message.content == process.env.FLAGASK){
 			message.channel.send(process.env.FLAG);
+		}else if(message.content == '^file'){
+			message.channel.send({files: ['./challenge.wav']});
 		}else if(Math.random()>0.95){
 			message.channel.send(responses[~~(Math.random()*responses.length)]);
 		}
